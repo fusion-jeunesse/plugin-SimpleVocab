f01956a53ede24f6efbfc03dcdea13f45eb4da73
Bigue Nique <BigueNique@gmail.com>
2019-09-27 21:10:36 -0400

    Enabled option groups in SimpleVocab fields
    Use "[ Group Label ]" or "--- Group Label ---" to organize values into groups.

--- SimpleVocabPlugin.php
+++ SimpleVocabPlugin.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * Simple Vocab
- * 
+ *
  * @copyright Copyright 2007-2012 Roy Rosenzweig Center for History and New Media
  * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU GPLv3
  */
 
 /**
  * The Simple Vocab plugin.
- * 
+ *
  * @package Omeka\Plugins\SimpleVocab
  */
 class SimpleVocabPlugin extends Omeka_Plugin_AbstractPlugin
@@ -20,12 +20,12 @@ class SimpleVocabPlugin extends Omeka_Plugin_AbstractPlugin
         'install', 'uninstall', 'initialize', 'upgrade',
         'define_acl', 'config_form', 'config',
     );
-    
+
     /**
      * @var array This plugin's filters.
      */
     protected $_filters = array('admin_navigation_main');
-    
+
     protected $_options = array(
         'simple_vocab_files' => 0,
     );
@@ -47,7 +47,7 @@ class SimpleVocabPlugin extends Omeka_Plugin_AbstractPlugin
         $db->query($sql);
         $this->_installOptions();
     }
-    
+
     /**
      * Uninstall this plugin.
      */
@@ -65,7 +65,7 @@ class SimpleVocabPlugin extends Omeka_Plugin_AbstractPlugin
             set_option('simple_vocab_files', $this->_options['simple_vocab_files']);
         }
     }
-    
+
     /**
      * Initialize this plugin.
      */
@@ -74,11 +74,11 @@ class SimpleVocabPlugin extends Omeka_Plugin_AbstractPlugin
         // Register the select filter controller plugin.
         $front = Zend_Controller_Front::getInstance();
         $front->registerPlugin(new SimpleVocab_Controller_Plugin_SelectFilter);
-        
+
         // Add translation.
         add_translation_source(dirname(__FILE__) . '/languages');
     }
-    
+
     public function hookConfigForm()
     {
         $view = get_view();
@@ -102,7 +102,7 @@ class SimpleVocabPlugin extends Omeka_Plugin_AbstractPlugin
         // Restrict access to super and admin users.
         $args['acl']->addResource('SimpleVocab_Index');
     }
-    
+
     /**
      * Add the Simple Vocab navigation link.
      */
