e9adb1f14e2734e116cda694b419248017204df2
Bigue Nique <BigueNique@gmail.com>
2019-09-27 22:16:18 -0400

    Added explanation for grouping terms

--- views/admin/index/index.php
+++ views/admin/index/index.php
@@ -46,8 +46,8 @@ jQuery(window).load(function () {
         </div>
         <div class="inputs six columns omega">
             <?php echo $this->formSelect('element_id', null, array('id' => 'element-id'), $options_for_select) ?>
-            <p class="explanation"><?php echo __('Select an element to manage its ' 
-            . 'custom vocabulary. Elements with a custom vocabulary are marked with ' 
+            <p class="explanation"><?php echo __('Select an element to manage its '
+            . 'custom vocabulary. Elements with a custom vocabulary are marked with '
             . 'an asterisk (*).'); ?></p>
         </div>
     </div>
@@ -55,24 +55,27 @@ jQuery(window).load(function () {
         <label for="terms"><?php echo __('Vocabulary Terms'); ?></label>
         <div class="inputs">
             <?php echo $this->formTextarea('terms', null, array('id' => 'terms', 'rows' => '10', 'disabled' => true)) ?>
-            <p class="explanation"><?php echo __('Enter the custom vocabulary ' 
-            . 'terms for this element, one per line. To delete the vocabulary, ' 
+            <p class="explanation"><?php echo __('Enter the custom vocabulary '
+            . 'terms for this element, one per line. To delete the vocabulary, '
             . 'simply remove the terms and submit this form.'); ?></p>
+            <p class="explanation"><?php echo __('To group terms under unselectable '
+            . 'labels, use the syntax "[ Group Label ]" '
+            . 'or "--- Group Label ---" (without quotes) on a separate line.'); ?></p>
         </div>
     </div>
-<p><?php echo __('%sClick here%s to display a list of texts for the selected element ' 
-. 'that currently exist in Omeka. You may use this list as a reference to build a ' 
-. 'vocabulary, but be aware of some caveats:', 
-'<a id="display-texts" href="#display-texts"><strong>', 
+<p><?php echo __('%sClick here%s to display a list of texts for the selected element '
+. 'that currently exist in Omeka. You may use this list as a reference to build a '
+. 'vocabulary, but be aware of some caveats:',
+'<a id="display-texts" href="#display-texts"><strong>',
 '</strong></a>'); ?></p>
 <ul>
     <li><?php echo __('Vocabulary terms must not contain newlines (line breaks).'); ?></li>
-    <li><?php echo __('Vocabulary terms are typically short and concise. If your ' 
-    . 'existing texts are otherwise, avoid using a controlled vocabulary for this ' 
+    <li><?php echo __('Vocabulary terms are typically short and concise. If your '
+    . 'existing texts are otherwise, avoid using a controlled vocabulary for this '
     . 'element.'); ?></li>
     <li><?php echo __('Vocabulary terms must be identical to their corresponding texts.'); ?></li>
-    <li><?php echo __('Existing texts that are not in the vocabulary will be ' 
-    . 'preserved — however, they cannot be selected in the item edit page, and ' 
+    <li><?php echo __('Existing texts that are not in the vocabulary will be '
+    . 'preserved — however, they cannot be selected in the item edit page, and '
     . 'will be deleted once you save the item.'); ?></li>
 </ul>
 </section>
